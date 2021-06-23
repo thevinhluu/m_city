97283ec4c7add76657d479f813d806cabf246b0e
Luu The Vinh <luuthevinh1988@gmail.com>
2021-06-23 14:38:12 +0700

    the sign in form

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Header from './components/Header_footer/header';
 import Footer from './components/Header_footer/footer';
 import Home from './components/Home';
+import SignIn from './components/Signin';
 
 const Routes = () => {
 	return (
 		<BrowserRouter>
 			<Header />
 			<Switch>
+				<Route path='/sign_in' exact component={SignIn} />
 				<Route path='/' exact component={Home} />
 			</Switch>
 			<Footer />
