49a49aa597908651c8c58d55337ad30098655b2a
Luu The Vinh <luuthevinh1988@gmail.com>
2021-06-23 23:49:52 +0700

    auth guards

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,6 +1,7 @@
 import { CircularProgress } from '@material-ui/core';
 import { useFormik } from 'formik';
 import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import * as Yup from 'yup';
 import { firebase } from '../../firebase';
 import { showErrorToast, showSuccessToast } from '../Utils/tools';
@@ -39,41 +40,47 @@ const SignIn = (props) => {
 	};
 
 	return (
-		<div className='container'>
-			<div className='signin_wrapper' style={{ margin: '100px' }}>
-				<form onSubmit={formik.handleSubmit}>
-					<h2>Please login</h2>
-					<input
-						name='email'
-						placeholder='Email'
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						value={formik.values.email}
-					/>
-					{formik.touched.email && formik.errors.email ? (
-						<div className='error_label'>{formik.errors.email}</div>
-					) : null}
+		<React.Fragment>
+			{!props.user ? (
+				<div className='container'>
+					<div className='signin_wrapper' style={{ margin: '100px' }}>
+						<form onSubmit={formik.handleSubmit}>
+							<h2>Please login</h2>
+							<input
+								name='email'
+								placeholder='Email'
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+								value={formik.values.email}
+							/>
+							{formik.touched.email && formik.errors.email ? (
+								<div className='error_label'>{formik.errors.email}</div>
+							) : null}
 
-					<input
-						placeholder='enter your password'
-						name='password'
-						type='password'
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						value={formik.values.password}
-					/>
-					{formik.touched.password && formik.errors.password ? (
-						<div className='error_label'>{formik.errors.password}</div>
-					) : null}
+							<input
+								placeholder='enter your password'
+								name='password'
+								type='password'
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+								value={formik.values.password}
+							/>
+							{formik.touched.password && formik.errors.password ? (
+								<div className='error_label'>{formik.errors.password}</div>
+							) : null}
 
-					{loading ? (
-						<CircularProgress color='secondary' className='progress' />
-					) : (
-						<button type='submit'>Log in</button>
-					)}
-				</form>
-			</div>
-		</div>
+							{loading ? (
+								<CircularProgress color='secondary' className='progress' />
+							) : (
+								<button type='submit'>Log in</button>
+							)}
+						</form>
+					</div>
+				</div>
+			) : (
+				<Redirect to={'/dashboard'} />
+			)}
+		</React.Fragment>
 	);
 };
 
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-import Header from './components/Header_footer/header';
+import Dashboard from './components/Admin/Dashboard';
 import Footer from './components/Header_footer/footer';
+import Header from './components/Header_footer/header';
 import Home from './components/Home';
 import SignIn from './components/Signin';
+import AuthGuard from './Hoc/Auth';
 
 const Routes = ({ user }) => {
 	return (
 		<BrowserRouter>
 			<Header user={user} />
 			<Switch>
-				<Route path='/sign_in' exact component={SignIn} />
+				<Route path='/dashboard' exact component={AuthGuard(Dashboard)} />
+				<Route path='/sign_in' exact component={(props) => <SignIn {...props} user={user} />} />
 				<Route path='/' exact component={Home} />
 			</Switch>
 			<ToastContainer />
