25938a4e9dc4320e1324985c29ed569d0650e7a0
Luu The Vinh <luuthevinh1988@gmail.com>
2021-06-25 06:26:14 +0700

    importing data to firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
+
+// import { cityDb } from './temp/m-city-export';
 
 const firebaseConfig = {
 	apiKey            : 'AIzaSyD0POfyqJBW5F2JndDkZITDHQCiGAsTL60',
@@ -12,5 +15,31 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+const DB = firebase.firestore();
+const matchesCollection = DB.collection('matches');
+const playersCollection = DB.collection('players');
+const positionsCollection = DB.collection('positions');
+const promotionsCollection = DB.collection('promotions');
+const teamsCollection = DB.collection('teams');
 
-export { firebase };
+// cityDb.matches.forEach((item) => {
+// 	matchesCollection.add(item);
+// });
+
+// cityDb.players.forEach((item) => {
+// 	playersCollection.add(item);
+// });
+
+// cityDb.positions.forEach((item) => {
+// 	positionsCollection.add(item);
+// });
+
+// cityDb.promotions.forEach((item) => {
+// 	promotionsCollection.add(item);
+// });
+
+// cityDb.teams.forEach((item) => {
+// 	teamsCollection.add(item);
+// });
+
+export { firebase, matchesCollection, playersCollection, positionsCollection, promotionsCollection, teamsCollection };
