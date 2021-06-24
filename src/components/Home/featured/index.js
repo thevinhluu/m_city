79c6abfb2851cb54c6b22d639f78281cdd989c5d
Luu The Vinh <luuthevinh1988@gmail.com>
2021-06-24 22:03:30 +0700

    finishing animations

--- src/components/Home/featured/index.js
+++ src/components/Home/featured/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Stripes from './Stripe';
+import Text from './Text';
 
 const Featured = () => {
 	return (
 		<div className='featured_wrapper'>
 			<Stripes />
+			<Text />
 		</div>
 	);
 };
