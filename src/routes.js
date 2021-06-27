67e7d5e6ff050f482e1b4dbb261c7b7aabac95fc
Luu The Vinh <luuthevinh1988@gmail.com>
2021-06-27 21:32:53 +0700

    admin players list

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Dashboard from './components/Admin/Dashboard';
+import AdminPlayers from './components/Admin/players';
 import Footer from './components/Header_footer/footer';
 import Header from './components/Header_footer/header';
 import Home from './components/Home';
@@ -14,6 +15,7 @@ const Routes = ({ user }) => {
 		<BrowserRouter>
 			<Header user={user} />
 			<Switch>
+				<Route path='/admin_players' exact component={AuthGuard(AdminPlayers)} />
 				<Route path='/dashboard' exact component={AuthGuard(Dashboard)} />
 				<Route path='/sign_in' exact component={(props) => <SignIn {...props} user={user} />} />
 				<Route path='/' exact component={Home} />
