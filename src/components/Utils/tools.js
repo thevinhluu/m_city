b5df99985e537ca9b97efbc09426f596060d34ff
Luu The Vinh <luuthevinh1988@gmail.com>
2021-06-28 10:49:12 +0700

    add and edit player

--- src/components/Utils/tools.js
+++ src/components/Utils/tools.js
@@ -1,3 +1,4 @@
+import { FormHelperText } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -75,4 +76,21 @@ export const logoutHandler = () => {
 		.catch((error) => {
 			showErrorToast(error.message);
 		});
+};
+
+export const textErrorHelper = (formik, values) => ({
+	error      : formik.errors[values] && formik.touched[values],
+	helperText : formik.errors[values] && formik.touched[values] ? formik.errors[values] : null
+});
+
+export const selectErrorHelper = (formik, values) => {
+	if (formik.errors[values] && formik.touched[values]) {
+		return <FormHelperText>{formik.errors[values]}</FormHelperText>;
+	}
+
+	return false;
+};
+
+export const selectIsError = (formik, values) => {
+	return formik.errors[values] && formik.touched[values];
 };
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Dashboard from './components/Admin/Dashboard';
 import AdminPlayers from './components/Admin/players';
+import AddEditPlayer from './components/Admin/players/addEditPlayers';
 import Footer from './components/Header_footer/footer';
 import Header from './components/Header_footer/header';
 import Home from './components/Home';
@@ -15,6 +16,8 @@ const Routes = ({ user }) => {
 		<BrowserRouter>
 			<Header user={user} />
 			<Switch>
+				<Route path='/admin_players/edit_player/:playerid' exact component={AuthGuard(AddEditPlayer)} />
+				<Route path='/admin_players/add_player' exact component={AuthGuard(AddEditPlayer)} />
 				<Route path='/admin_players' exact component={AuthGuard(AdminPlayers)} />
 				<Route path='/dashboard' exact component={AuthGuard(Dashboard)} />
 				<Route path='/sign_in' exact component={(props) => <SignIn {...props} user={user} />} />
