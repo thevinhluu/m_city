9d9a748da05c7b4be4b57fef22d5e7749ff97cf7
Luu The Vinh <luuthevinh1988@gmail.com>
2021-06-29 10:12:51 +0700

    finish the team section

--- src/components/theTeam/index.js
+++ src/components/theTeam/index.js
@@ -1,9 +1,10 @@
+import { CircularProgress } from '@material-ui/core';
+import { Promise } from 'core-js';
 import React, { useEffect, useState } from 'react';
-import PlayerCar from '../Utils/playerCard';
 import { Slide } from 'react-awesome-reveal';
 import { firebase, playersCollection } from '../../firebase';
+import PlayerCard from '../Utils/playerCard';
 import { showErrorToast } from '../Utils/tools';
-import { Promise } from 'core-js';
 
 const TheTeam = () => {
 	const [loading, setLoading] = useState(true);
@@ -56,9 +57,52 @@ const TheTeam = () => {
 		[players]
 	);
 
-	console.log(players);
+	const showPlayerByCategory = (category) =>
+		players
+			? players.map((player, i) => {
+					return player.position === category ? (
+						<Slide left key={player.id} triggerOnce>
+							<div className='item'>
+								<PlayerCard
+									number={player.number}
+									name={player.name}
+									lastname={player.lastname}
+									bck={player.url}
+								/>{' '}
+							</div>
+						</Slide>
+					) : null;
+				})
+			: null;
 
-	return <div>the team</div>;
+	return (
+		<div className='the_team_container'>
+			{loading ? (
+				<div className='progress'>
+					<CircularProgress />
+				</div>
+			) : (
+				<div>
+					<div className='team_category_wrapper'>
+						<div className='title'>Keepers</div>
+						<div className='team_cards'>{showPlayerByCategory('Keeper')}</div>
+					</div>
+					<div className='team_category_wrapper'>
+						<div className='title'>Defence</div>
+						<div className='team_cards'>{showPlayerByCategory('Defence')}</div>
+					</div>
+					<div className='team_category_wrapper'>
+						<div className='title'>Midfield</div>
+						<div className='team_cards'>{showPlayerByCategory('Midfield')}</div>
+					</div>
+					<div className='team_category_wrapper'>
+						<div className='title'>Striker</div>
+						<div className='team_cards'>{showPlayerByCategory('Striker')}</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default TheTeam;
