fedbe6f7276f38e2bb9975d6fdea3ed1d9a910a5
Luu The Vinh <luuthevinh1988@gmail.com>
2021-06-29 10:47:59 +0700

    admin matches list

---
+++ src/components/Admin/matches/addEditMatch.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddEditMatch = () => {
+	return <div>edit add match</div>;
+};
+
+export default AddEditMatch;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Dashboard from './components/Admin/Dashboard';
+import AddEditMatch from './components/Admin/matches/addEditMatch';
+import AdminMatches from './components/Admin/matches/index';
 import AdminPlayers from './components/Admin/players';
 import AddEditPlayer from './components/Admin/players/addEditPlayers';
 import Footer from './components/Header_footer/footer';
@@ -17,9 +19,14 @@ const Routes = ({ user }) => {
 		<BrowserRouter>
 			<Header user={user} />
 			<Switch>
+				<Route path='/admin_matches/edit_match/:matchid' exact component={AuthGuard(AddEditMatch)} />
+				<Route path='/admin_matches/add_match' exact component={AuthGuard(AddEditMatch)} />
+				<Route path='/admin_matches' exact component={AuthGuard(AdminMatches)} />
+
 				<Route path='/admin_players/edit_player/:playerid' exact component={AuthGuard(AddEditPlayer)} />
 				<Route path='/admin_players/add_player' exact component={AuthGuard(AddEditPlayer)} />
 				<Route path='/admin_players' exact component={AuthGuard(AdminPlayers)} />
+
 				<Route path='/dashboard' component={AuthGuard(Dashboard)} />
 				<Route path='/the_team' component={TheTeam} />
 				<Route path='/sign_in' exact component={(props) => <SignIn {...props} user={user} />} />
