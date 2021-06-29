733cb290d99d3d4434d0c808fd5594372f437a37
Luu The Vinh <luuthevinh1988@gmail.com>
2021-06-29 20:12:36 +0700

    filter matches

--- src/components/theMatches/index.js
+++ src/components/theMatches/index.js
@@ -9,6 +9,16 @@ import MatchesList from './matchesList';
 
 const TheMatches = () => {
 	const [matches, setMatches] = useState(null);
+	const [state, dispatch] = useReducer(
+		(prevState, nextState) => {
+			return { ...prevState, ...nextState };
+		},
+		{
+			filterMatches : null,
+			playedFilter  : 'All',
+			resultFilter  : 'All'
+		}
+	);
 
 	useEffect(
 		() => {
@@ -21,21 +31,105 @@ const TheMatches = () => {
 							...doc.data()
 						}));
 						setMatches(matches);
+						dispatch({ ...state, filterMatches: matches });
 					})
 					.catch((error) => {
 						showErrorToast(error);
 					});
 			}
 		},
-		[matches]
+		[matches, state]
 	);
 
+	const showPlayed = (played) => {
+		// all, yes, no
+		const list = matches.filter((match) => {
+			return match.final === played;
+		});
+
+		dispatch({
+			...state,
+			filterMatches : played === 'All' ? matches : list,
+			playedFilter  : played,
+			resultFilter  : 'All'
+		});
+	};
+
+	const showResult = (result) => {
+		const list = matches.filter((match) => {
+			return match.result === result;
+		});
+
+		dispatch({
+			...state,
+			filterMatches : result === 'All' ? matches : list,
+			playedFilter  : 'All',
+			resultFilter  : result
+		});
+	};
+
 	return (
-		<div>
+		<React.Fragment>
 			{matches ? (
 				<div className='the_matches_container'>
 					<div className='the_matches_wrapper'>
-						<div className='left'>list</div>
+						<div className='left'>
+							<div className='match_filters'>
+								<div className='match_filters_box'>
+									<div className='tag'>Show matches</div>
+									<div className='cont'>
+										<div
+											className={`option ${state.playedFilter === 'All' ? 'active' : ''}`}
+											onClick={() => showPlayed('All')}
+										>
+											All
+										</div>
+										<div
+											className={`option ${state.playedFilter === 'yes' ? 'active' : ''}`}
+											onClick={() => showPlayed('yes')}
+										>
+											Played
+										</div>
+										<div
+											className={`option ${state.playedFilter === 'no' ? 'active' : ''}`}
+											onClick={() => showPlayed('no')}
+										>
+											Not Played
+										</div>
+									</div>
+								</div>
+								<div className='match_filters_box'>
+									<div className='tag'>Result games</div>
+									<div className='cont'>
+										<div
+											className={`option ${state.resultFilter === 'All' ? 'active' : ''}`}
+											onClick={() => showResult('All')}
+										>
+											All
+										</div>
+										<div
+											className={`option ${state.resultFilter === 'W' ? 'active' : ''}`}
+											onClick={() => showResult('W')}
+										>
+											W
+										</div>
+										<div
+											className={`option ${state.resultFilter === 'L' ? 'active' : ''}`}
+											onClick={() => showResult('L')}
+										>
+											L
+										</div>
+										<div
+											className={`option ${state.resultFilter === 'D' ? 'active' : ''}`}
+											onClick={() => showResult('D')}
+										>
+											D
+										</div>
+									</div>
+								</div>
+							</div>
+							<MatchesList matches={state.filterMatches} />
+						</div>
 						<div className='right'>
 							<LeagueTable />
 						</div>
@@ -46,7 +140,7 @@ const TheMatches = () => {
 					<CircularProgress />
 				</div>
 			)}
-		</div>
+		</React.Fragment>
 	);
 };
 
